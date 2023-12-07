049211f4ce55b3d06fd1af56924db1d6d4391293
TheBroOfCookies <345hot345@hotmail.no>
2023-12-07 15:35:34 +0100

    Day 07 task 2

---
+++ Day_07/day_07_2.py
@@ -0,0 +1,76 @@
+card_to_sortable = {"2":"a", "3":"b", "4":"c", "5":"d", "6":"e", "7":"f", "8":"g", "9":"h", "T":"i", "J":"1", "Q":"k", "K":"l", "A":"m"} #solution: 253630098
+
+class HandObj:
+    def __init__(self, hand, bid, type, sortable):
+        self.hand = hand
+        self.bid = bid
+        self.type = type
+        self.sortable = sortable
+    
+    def __str__(self):
+        return f"Hand: {self.hand}, Bid: {self.bid}, Type: {self.type}"
+
+def find_total_winnings(filename):
+    with open(filename) as file:
+        lines = file.readlines()
+        hands_ordered = []
+        for line in lines:
+            hand, bid = line.strip().split()
+            type = find_type_from_hand(hand)
+            sortable = hand_to_sortable(hand)
+            hands_ordered.append(HandObj(hand, int(bid), type, sortable))
+            
+        hands_ordered = sorted(hands_ordered, key=lambda x:x.sortable)
+        hands_ordered = sorted(hands_ordered, key=lambda x:x.type)
+        for hand in hands_ordered:
+            print(hand, end =" ")
+        print()
+            
+        sum_of_hands = 0
+        for i in range(len(hands_ordered)):
+            sum_of_hands += hands_ordered[i].bid * (i+1)
+        return sum_of_hands
+            
+def find_type_from_hand(hand):
+    count = [[hand[0], 1], [hand[1], 1], [hand[2], 1], [hand[3], 1], [hand[4], 1]]
+    jokers = 0
+    for i in range(len(hand)):
+        if hand[i] == "J":
+            jokers += 1
+        elif hand[i] == count[0][0] and i > 0:
+            count[0][1] += 1
+        elif hand[i] == count[1][0] and i > 1:
+            count[1][1] += 1
+        elif hand[i] == count[2][0] and i > 2:
+            count[2][1] += 1
+        elif hand[i] == count[3][0] and i > 3:
+            count[3][1] += 1
+
+    count = sorted(count, key=lambda x: x[1]) #sorted so that last element (count[4]) contains the most prominent card
+    if jokers == 5:   #all joker edge case
+        return 6
+    count[4][1] += jokers #convert jokers to most prominent card
+
+    if count[4][1] == 5:                        #6: five of a kind
+        return 6
+    elif count[4][1] == 4:                      #5: four of a kind
+        return 5
+    elif count[4][1] == 3 and count[3][1] == 2: #4: full house
+        return 4
+    elif count[4][1] == 3:                      #3: three of a kind
+        return 3
+    elif count[4][1] == 2 and count[3][1] == 2: #2: two pair
+        return 2
+    elif count[4][1] == 2:                      #1: one pair
+        return 1
+    else:                                       #0: high card
+        return 0
+    
+def hand_to_sortable(hand):
+    sortable = ""
+    for card in hand:
+        sortable += card_to_sortable[card]
+    return sortable
+
+#print(find_total_winnings("example.txt"))
+print(find_total_winnings("input.txt"))
